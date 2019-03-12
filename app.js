5fec5e166fea7ace05b872b485372008f4682d39
alexgQQ <alex.grand@beatport.com>
2019-03-11 20:41:01 -0600

    Tweak donuts :p

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var grid = new contrib.grid({rows: 2, cols: 2, screen: screen})
 
 var donut = grid.set(1, 1, 1, 1, contrib.donut,
     {
-    label: 'Memory',
-    radius: 16,
+    label: 'Memory Usage',
+    radius: 10,
     arcWidth: 4,
     yPadding: 2,
   data: [
-    {percent: 80, label: 'USED RAM', color: 'green'}
+    {percent: 0, label: '', color: 'green'}
   ]
 });
 
@@ -35,21 +35,19 @@ setInterval(updateLog, 500)
 var foo = 0.00;
 
 function updateDonut(){
-    // si.mem()
-    //     .then(function(data) {
-    //         total = data['total']
-    //         used = data['used']
-    //         foo = used/total
-    //     }, function(error) {
-    //         console.error(error)
-    //     });
-    if (foo > 256) foo = 0;
+    si.mem()
+        .then(function(data) {
+            total = data['total']
+            used = data['used']
+            foo = used/total
+        }, function(error) {
+            console.error(error)
+        });
 
     donut.update([
-        {percent: parseFloat((foo/256) % 1).toFixed(2), label: 'USED RAM', 'color': [foo, foo, foo]},
+        {percent: parseFloat((foo) % 1).toFixed(2), label: '', 'color': 'green'},
     ])
     screen.render();
-    foo += 1
 }
 
 function updateLog(){
